1d70cd4af743836cbcdee14a94e6e7c77e84fe5c
Pranav S <pranav.s.cd.mat20@itbhu.ac.in>
2022-06-10 12:05:23 +0530

    Update

---
+++ runfile.java
@@ -0,0 +1,115 @@
+import java.util.*;
+
+public class runfile {
+    public static void main(String[] args) {
+        System.out.println("!!Results published!!\nBest of luck\n");
+        Result Ned = new Result("Rohan", 2, 3, "Physics", true, false, 50);
+        Ned.displayResult();
+        System.out.println("");
+        Result Sam = new Result("Suresh", 57, 4, "Maths", false, true, 49);
+        Sam.displayResult();
+        System.out.println("");
+        Result John = new Result("Pranav", 56, 2, "Chemistry", true, false, 40);
+        John.displayResult();
+    }
+}
+
+// interface for defining the rewards and penalty marks
+interface Achievement {
+    static int grace = 10;
+    static int penalty = -5;
+}
+
+// Registration class, serves as the superclass for Student
+abstract class Registration {
+    public String name;
+    public int year;
+    public String subject;
+
+    abstract void displayStudent();
+
+}
+
+// once registered, a professor and roll number is assigned. Student class
+// is the subclass of registration
+class Student extends Registration {
+    // Professor XYZ is assigned for math course
+    // Professor ABC is assigned otherwise
+    public String professor;
+    public int roll;
+
+    public void assignProf() {
+        if (subject == "Physics")
+            professor = "K S Jain";
+        else if ( subject == "Chemistry")
+            professor = "Divakar";
+        else
+            professor = "A J Singh";
+    }
+
+    // method to view information about the student
+    public void displayStudent() {
+        System.out.println(name);
+        System.out.println("Roll no.: " + roll);
+        System.out.println("Current year: " + year);
+        System.out.println("Subject: " + subject);
+        System.out.println("Professor: " + professor);
+    }
+}
+
+// The Subclass of Student, Examprofile serves as a class to
+// confirm if a student has achievement or penalties to be added
+// to the result
+class ExamProfile extends Student {
+    public boolean achieved;
+    public boolean punished;
+    public int score;
+
+    // method to view score of the student in exam
+    public void showScore() {
+        System.out.println("Score in exam: " + score);
+        if (punished)
+            System.out.println("!!Punished!!\nNot eligible for any bonous marks\nPenalty applied");
+        else if (achieved)
+            System.out.println("Extra curricular bonous added");
+    }
+}
+
+// Result calculates the final marks obtained by the student
+class Result extends ExamProfile implements Achievement {
+    public int marks;
+
+    // constructor
+    Result(String name, int roll, int year, String subject, boolean achieved, boolean punished, int score) {
+        this.name = name;
+        this.roll = roll;
+        this.year = year;
+        this.subject = subject;
+        this.achieved = achieved;
+        this.punished = punished;
+        this.score = score;
+    }
+
+    // method to calculate the final marks scored by the student
+    public void calculatedResult() {
+        if (punished) {
+            marks = score + penalty;
+        } else if (achieved) {
+            marks = score + grace;
+        } else {
+            marks = score;
+        }
+    }
+
+    // method to display the final result of the student
+    public void displayResult() {
+        assignProf();
+        displayStudent();
+        showScore();
+        calculatedResult();
+        System.out.print("Total Marks: ");
+        System.out.println(marks);
+
+    }
+
+}
